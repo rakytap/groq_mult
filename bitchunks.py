896ce8b169f2a1df69fa10f910779d92bdcf684a
Peter Rakyta <peter.rakyta@tk.elte.hu>
2022-06-17 20:53:03 +0000

    add function to divide into bitchunks

---
+++ bitchunks.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+
+bitchunk = 7
+num_of_chunks = 2
+full_bitwidth = num_of_chunks*bitchunk
+bound = pow(2, full_bitwidth)
+#bound = 20
+print(bound)
+
+# function to split higher bitwidth array into int8 arrays bit-slices
+def divide_into_bitchunks(inp, num_of_chunks):
+
+    # determine the dimensions of the return
+    shape = tuple( [num_of_chunks] + list(inp.shape))
+
+    sign_bits = np.sign(inp)
+    sign_bits = (np.abs(sign_bits) - sign_bits)/2
+    sign_bits = sign_bits.astype(np.uint8)
+
+    #print(inp)
+    inp_loc = inp.copy().astype(np.uint32)
+    #print(inp_loc)
+
+
+
+    # reserve space for the return
+    ret = np.zeros(shape, dtype=np.int8)
+    
+    
+    for chunk in range(num_of_chunks):
+
+        # extract decimal bits
+        for bit_idx in range(bitchunk):
+            bit = (inp_loc % 2)
+            ret[chunk] += pow(2,bit_idx)*bit
+            inp_loc = np.right_shift(inp_loc,1)
+
+        
+        # adding sign bit
+        if( chunk == num_of_chunks-1) :
+            ret[chunk] += pow(2,bitchunk)*sign_bits
+                
+
+    return ret
+
+
+t1_data = np.random.randint(-bound, bound, (1, 50), dtype=np.int16) # the lower 8 bits of 16 bits integers
+t2_data = np.random.randint(-bound, bound, (50, 50), dtype=np.int16) # the lower 8 bits of 16 bits integers
+
+
+mult_result_16 = np.matmul(t1_data, t2_data)
+#print(mult_result_16)
+
+
+t1_data_8 = divide_into_bitchunks(t1_data, num_of_chunks)
+t1_data_16 = t1_data_8[0,:].astype(np.int16) + pow(2,bitchunk)*t1_data_8[1,:].astype(np.int16)
+print(np.allclose(t1_data, t1_data_16, rtol=1e-1, atol=1e-1, equal_nan=True))
+
+
+
+t2_data_8 = divide_into_bitchunks(t2_data, num_of_chunks)
+t2_data_16 = t2_data_8[0,:].astype(np.int16) + pow(2,bitchunk)*t2_data_8[1,:].astype(np.int16)
+print(np.allclose(t2_data, t2_data_16, rtol=1e-1, atol=1e-1, equal_nan=True))
